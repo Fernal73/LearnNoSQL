e5e53cf37d6baebc087185905c7ffbb08d2b77fc
fernal73 <linus.fernandes@gmail.com>
2021-04-25 17:35:34 +0530

    Add more commands

--- mongodb/tutorial.js
+++ mongodb/tutorial.js
@@ -14,3 +14,15 @@ let db = conn.getDB("myDatabase");
 printObj(db);
 printObj(db.adminCommand("listDatabases"));
 
+db = db.getSiblingDB("test");
+printObj(db);
+printObj(db.collection.save( { a: 1 }));
+
+printObj(db.getCollectionNames());
+
+var cursor = db.collection.find();
+var documentArray = cursor.toArray();
+var doc = documentArray[0];
+printObj(doc);
+
+db = db.getSiblingDB("sampleDB");
