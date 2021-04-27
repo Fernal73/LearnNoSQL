eb78d654c865d5aa1311457e1073594442e11d09
fernal73 <linus.fernandes@gmail.com>
2021-04-28 04:47:16 +0530

    Update crud.js

--- mongodb/crud.js
+++ mongodb/crud.js
@@ -19,8 +19,17 @@ db.people.save({
 });
 
 db.people.insertMany([{
+  name: "Thomas",
+  age: 45
+}, {
   name: "Tim",
   age: 28
+}, {
+  name: "Jim",
+  age: 28
+}, {
+  name: "Jim",
+  age: 38
 }, {
   name: "John",
   age: 25
@@ -28,5 +37,11 @@ db.people.insertMany([{
   name: "Kathy",
   age: 23
 }]);
+
+//Will replace only first matching
+db.people.updateOne({name: "Thomas"},{$set: {age: 69, name: "Tom"}});
+db.people.updateMany({name: "Jim"},{$set:{name: "James"}});
+db.people.updateMany({name: "Kathy"},{$set:{age: 30, salary:50000}});
+db.people.replaceOne({name:"John"}, {name:"Lakmal",age:25,address:"Sri Lanka"});
 db.people.find().forEach(printjson);
 db.people.drop();
